5f3cc378b0d96b72c28a861a57eb150a41bd4575
danielmar18 <33004159+danielmar18@users.noreply.github.com>
2018-10-10 15:43:29 +0000

    Additions to testers

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,13 +1,20 @@
-function add (number){
+function add(number){
     if(number == 0)
     return 0;
 
-    if(number.includes(",")){
-        var numberArray = number.split(",")
-        return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+    if(number.includes(",") || number.includes("\n")){
+        var numberArray = number.split(/[\n,]+/);
+        return sum(numberArray);
     }
     else
         return parseInt(number);
 }
 
+function sum(numberArray){
+    var total = 0;
+    for(var i = 0; i < numberArray.length; i++){
+        total += parseInt(numberArray[i]);
+    }
+    return total;
+}
 module.exports = add;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -11,3 +11,11 @@ it("Should return number when only one number is in the string", () => {
 it("Should return sum of two numbers given", () => {
     expect(add("1,4")).toBe(5);
 });
+
+it("Should summize multiple numbers", () => {
+    expect(add("1,2,4,6")).toBe(13);
+});
+
+it("Should split on newline", () => {
+    expect(add("6\n2")).toBe(8);
+});
