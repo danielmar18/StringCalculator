e43941843759a02b3f0604bb483988b41d0e6371
danielmar18 <33004159+danielmar18@users.noreply.github.com>
2018-10-10 15:08:47 +0000

    Inital testing tests

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,5 +1,8 @@
 function add (number){
+    if(number == 0)
     return 0;
+
+    return parseInt(number);
 }
 
 module.exports = add;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require ("./StringCalculator");
 it("Should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+it("Should return number when only one number is in the string", () => {
+    expect(add("6")).toBe(6);
+});
